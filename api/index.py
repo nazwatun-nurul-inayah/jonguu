2a860c76ab7a3e38314a25a47c1146dc07aa4c6b
nazwatun-nurul-inayah <pkm26@teknokrat.ac.id>
2025-07-14 16:54:13 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -129,7 +129,7 @@ class ArtikelAPI:
 
 
 config_artikel = ArtikelAPI()
-config_artikel.sheet_id('14EXDQFZ3w3m20W1w3EBl0MGCO9hxNz63ZWhC4tQewYY')
+config_artikel.sheet_id('1DRDJmqqVq5f9gOLNo-AOFwAICnczeVD4RqffSdX5osY')
 config_artikel.gid('710523043')
 
 
